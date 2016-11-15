412cd98757dbf8b9d08bc1ef4d51c9200ae5120b
Matthew <matthewmazz12@gmail.com>
2016-11-15 17:48:16 -0500

    Still failing to commit

---
+++ src/input/input.c
@@ -0,0 +1,98 @@
+#include "../quadsolver.h"
+
+/*
+ takeInputs function is called in main. It gains input from 
+ the command line utilizing a char array and fgets. This is 
+ how we get A, B, and C. These floats will then be validated 
+ by an outside function. Once validated the float will be 
+ send to qsolv.
+ */
+qInputs 
+takeInputs(char* readin)
+{
+	double a;
+	double b;
+	double c;
+	char* end;
+
+	qInputs inputs;
+	char strA[256/3];
+	char strB[256/3];
+	char strC[256/3];
+	char strValidate[256/3]; // Should always be empty
+		
+
+	memset(strA, ' ', 256/3);
+	memset(strB, ' ', 256/3);
+	memset(strC, ' ', 256/3);
+	memset(strValidate, ' ', 256/3);
+
+	sscanf(readin, "%s %s %s %s", strA, strB, strC, strValidate);
+	
+	// More than 3 parameters given
+	 if (strValidate[0] != ' ') {
+	 	// Error
+		inputs.errorCode = -2;
+		return inputs;
+	 }
+
+	a = strtod(strA, &end);
+	if(validateInput(strA) != 0) {
+		inputs.errorCode = -1;
+		return inputs;
+	}
+
+	b = strtod(strB, &end);
+	if(validateInput(strB) != 0) {
+		inputs.errorCode = -1;
+		return inputs;
+	}
+
+	c = strtod(strC, &end);
+	if(validateInput(strC) != 0) {
+		inputs.errorCode = -1;
+		return inputs;
+	}
+
+	inputs.a = a;
+	inputs.b = b;
+	inputs.c = c;
+
+	// Return to main to run qsolv
+	return inputs;
+}
+
+/*
+ May not use this function -- just use redirection instead. 
+*/
+
+// int 
+// runFile(char * fname)
+// {
+// 	FILE * fp;
+    
+// 	if(!(fp = fopen(fname,"r"))) return -1;
+	
+// 	char * token;
+// 	float a, b, c;
+// 	char readin[256];
+
+// 	while(fgets(readin,sizeof(readin),fp))
+//     {
+// 		token = strtok(readin," ");
+// 		a = atof(token);
+// 		token = strtok(NULL," ");
+// 		b = atof(token);
+// 		token = strtok(NULL," ");
+// 		c = atof(token);
+
+// 		printf("found %5.5f %5.5f %5.5f \n",a,b,c);
+//         //call validate(? may be moved to main)
+//         //call qsolv(? may be moved to main)
+// 	}
+
+// 	fclose(fp);
+
+// 	return 0;
+
+// }
--- src/qs/qs.c
+++ src/qs/qs.c
@@ -1,126 +1,43 @@
 /* This program will read floats from command line inputs or read from a file
 in order to solve the quadratic equation. Inputs are taken in float form and 
 calculated using double precesion to IEEE standard.*/
+#include "../quadsolver.h"
 
-#include <stdio.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
 
-//function definitions
-int takeInputs();
-int runFile(char * fname);
-char *remove_newline(char *s);
 
 int 
 main(int argc, char * argv[])
 {
-	//Inputs from command line
-	if(argc < 2)
-      {//Start loop here instead of in takeInput
-		if(takeInputs() == 0) return 0;
-        
-		else return -1;
-	  }
-    //Inputs from file
-	else if(argc == 2)
-      {
-		char * f = argv[1];
-		if(runFile(f) == 0) return 0; 
-		else return -1;
-	  }
-    //Input error
-	else
-      {
-        printf("Incorrect number of inputs. Please read readme for useage.\n");
-	  }
-	
-	return -2; //shouldn't reach this point
-}
+	qInputs input;
 
-/*
- takeInputs function is called in main. It gains input from 
- the command lineutilizing a char array and fgets. This is 
- how we get A, B, and C. These floats will then be validated 
- by an outside function. Once validated the float will be 
- send to qsolv.
- */
-int 
-takeInputs()
-{
-	char s[100];
-	char * p = s;
-	float a;
-	float b;
-	float c;
-
-	for(;;)
-      {
-
-		printf("Enter your floats for the quadratic equation: \n");
-		printf("A: ");
-		fgets(s, sizeof(s), stdin);
-		p = remove_newline(p);
-		a = atof(p);
-
-		printf("B: ");
-		fgets(s, sizeof(s), stdin);
-		p = remove_newline(p);
-		b = atof(p);
-
-		printf("C: ");
-		fgets(s, sizeof(s), stdin);
-		p = remove_newline(p);
-		c = atof(p);
-
-		printf("found %5.5f %5.5f %5.5f \n",a,b,c);
-        //call validate(? may be moved to main)
-		//call qsolv(? may be moved to main)
-
-	  }
-	return 0;
-}
-
-
-int 
-runFile(char * fname)
-{
-	FILE * fp;
-    
-	if(!(fp = fopen(fname,"r"))) return -1;
-	
-	char * token;
-	float a, b, c;
 	char readin[256];
 
-	while(fgets(readin,sizeof(readin),fp))
-    {
-		token = strtok(readin," ");
-		a = atof(token);
-		token = strtok(NULL," ");
-		b = atof(token);
-		token = strtok(NULL," ");
-		c = atof(token);
+	printf("Please enter a,b, and c separated by spaces\n (i.e., <a> <b> <c>) or \"q\" to quit\n");
+	//Inputs from command line
+	while(fgets(readin,sizeof(readin), stdin)) {
 
-		printf("found %5.5f %5.5f %5.5f \n",a,b,c);
-        //call validate(? may be moved to main)
-        //call qsolv(? may be moved to main)
+		// Quit command
+		if(strA[0] == 'q' ) {
+			printf("Quitting...\n");
+			exit(0);
+		}
+
+		input = takeInputs(readin);
+		if (input.errorCode != 0) {
+			// Raise error
+			// Output should handle error code
+			printf("Error, please try again");
+		}
+
+		// We're good
+		else {
+			printf("Running qusolve for a=%d, b=%d, c=%d", input.a, input.b, input.c);
+			//qsolve(input.a, input.b, input.c);
+
+			// End-- run loop again
+			printf("Please enter a,b, and c separated by spaces\n (i.e., <a> <b> <c>) or \"q\" to quit\n");
+		}
 	}
-
-	fclose(fp);
-
-	return 0;
-
-}
-
-char *
-remove_newline(char *s)
-{
-    int len = strlen(s);
-
-    if (len > 0 && s[len-1] == '\n') s[len-1] = '\0';
-            // if there's a newline, truncate the string
-
-    return s;
+	
+	return -2; //shouldn't reach this point
 }
